fca909f45368ad15e9a6fee3ecbe03da9132fca3
MJ12358 <wtpltlf.dev@gmail.com>
2023-02-09 17:03:56 -0600

    modify example

--- example/main.dart
+++ example/main.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_extensionz/flutter_extensionz.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () => context.showSnackBar(const Text('The snack bar text')),
-      icon: const Icon(Icons.person),
+    return const MaterialApp(
+      home: Home(),
+      localizationsDelegates:
+          FlutterExtensionzLocalizations.localizationsDelegates,
+      restorationScopeId: 'app',
+      supportedLocales: FlutterExtensionzLocalizations.supportedLocales,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final String text = context.flutterExtensionzLocalizations.accept;
+
+    return Scaffold(
+      body: Center(
+        child: IconButton(
+          onPressed: () => context.showSnackBar(Text(text)),
+          icon: const Icon(Icons.home),
+        ),
+      ),
     );
   }
 }
